ddb3bf7bc346cb16e97413259f6fe3df908b2dfe
Mr Grey <ksmaybe@gmail.com>
2018-07-01 20:21:00 -0400

    edited

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -1,1 +1,11 @@
-asdfads
+#include <iostream>
+
+using namespace std;
+
+int main()
+{	
+	constexpr int WORD_LENGTH = 5;
+	cout << "Hello and welcome to myshit\n";
+	cout << "Take a dumb guess at a " << WORD_LENGTH << "-letter isogram, asshole!\n";
+	return 0;
+}
